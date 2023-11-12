f482960df316039c016a337103f868a30ac0931b
minsu20 <syt07100@gmail.com>
2023-11-12 16:43:38 +0900

    [backend] feat: 아이디 중복 체크 추가

---
+++ backend/src/main/java/com/moing/bo/backend/domain/admin/application/dto/response/CheckIdResponse.java
@@ -0,0 +1,14 @@
+package com.moing.bo.backend.domain.admin.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIdResponse {
+    private Boolean isDuplicated;
+}
